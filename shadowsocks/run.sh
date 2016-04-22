b30319de50b765da244994a05cf6791795f5623e
xyx <xieyx@qssec.com>
2016-04-22 11:53:21 +0800

    run.sh

--- shadowsocks/run.sh
+++ shadowsocks/run.sh
@@ -1,4 +1,5 @@
+#/bin/sh
 password=$(cat /dev/urandom | head -n 10 | md5sum | head -c 10)
 sed -i "s#PASSWORD#$password#g" /etc/shadowsocks.json
 echo "shadowsocks password is $password"
-/usr/local/bin/ssserver -c etc/shadowsocks.json d start
+/usr/local/bin/ss-server -c etc/shadowsocks.json d start
